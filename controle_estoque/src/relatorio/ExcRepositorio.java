310e56641fe86c9c00e7264fe6cd57ed0bbc0f7d
martinspsj <martinspsj@10d4effa-1035-11df-a8dc-870042ab4e57>
2010-02-17 20:05:07 +0000

    foi inserido os dados da tabela de cliente no jtable

---
+++ controle_estoque/src/relatorio/ExcRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package relatorio;
+
+/**
+ *
+ * @author martins
+ */
+public class ExcRepositorio extends Exception {
+public ExcRepositorio(String mensagem) {
+			super(mensagem);
+	}
+}
