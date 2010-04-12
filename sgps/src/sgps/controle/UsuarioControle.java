3382879fe77a1191ab90322c45072d45c8a115b5
martinspsj <martinspsj@10d4effa-1035-11df-a8dc-870042ab4e57>
2010-04-12 05:18:51 +0000

    implementado o crud de usuario

--- sgps/src/sgps/controle/UsuarioControle.java
+++ sgps/src/sgps/controle/UsuarioControle.java
@@ -2,20 +2,20 @@
  * Classe responsável por fornecer uma instância do Usuario, gerenciar a
  * persistencia do Usuario e controlar as regras de negócio referente ao
  * Usuario
- * @author Daniel Vilas Boas
+ * @author Martins Pereira
  */
 package sgps.controle;
 
 import sgps.classeMapeada.Usuario;
 
 public class UsuarioControle extends ControleAbstrato<Usuario, Integer> {
-    
+
     /** Construtor da classe UsuarioControle. */
     public UsuarioControle() {
         super(Usuario.class);
-  
+
     }
-   
+
     /**
      * Método responsável por retornar uma instância de Usuario.
      * @return Usuario - Instância de Usuario
@@ -23,5 +23,5 @@ public class UsuarioControle extends ControleAbstrato<Usuario, Integer> {
     public Usuario getInstanciaUsuario() {
         return new Usuario();
     }
-    
+
 }
